b6267031717a295999b58465e70da7e6263f18f3
Felipe Coury <felipe.coury@gmail.com>
2019-05-30 11:55:46 -0300

    refactor: made it into a standalone webapp

--- index.js
+++ index.js
@@ -1,67 +1,91 @@
-const fs = require('fs');
-const data = JSON.parse('[' + fs.readFileSync('data.json', 'utf8').replace(/([a-z]):/g, '"$1":') + ']');
-
-const rows = ['B0', 'B1', 'B2', 'B3', 'B7'];
-const cols = ['D0', 'D1', 'D2', 'D3', 'D5', 'D4', 'D6', 'D7', 'B4', 'F7', 'F6', 'F5', 'F4', 'F1', 'F0'];
-
 class Key {
   constructor(x, y, data) {
     const [col, row] = data.split('\n');
 
     this.posCol = x;
     this.posRow = y;
-    this.matCol = Array.from(cols).indexOf(col);
-    this.matRow = Array.from(rows).indexOf(row);
+    this.matCol = Array.from(Key.cols).indexOf(col);
+    this.matRow = Array.from(Key.rows).indexOf(row);
+    console.log('key',
+      this.posCol,
+      this.posRow,
+      this.matCol,
+      this.matRow,
+    );
   }
 }
 
-const keys = [];
-data.forEach((r, y) => {
-  r.forEach((key, x) => {
-    if (!key || (typeof key !== 'object')) {
-      keys.push(new Key(x, y, key));
-    }
-  });
-});
+function run(_rows, _cols, kleStr) {
+  const cols = _cols.split(',').map(x => x.trim());
+  const rows = _rows.split(',').map(x => x.trim());
 
-const matrix = [];
-let matrixStr = '';
-for (let r = 0; r < rows.length; r++) {
-  for (let c = 0; c < cols.length; c++) {
-    const key = keys.find(k => k.matCol === c && k.matRow === r);
-    if (key) {
-      key.name = `k${r.toString(16)}${c.toString(16)}`;
-      matrixStr += `${key.name}, `;
-    } else {
-      matrixStr += `_x_, `;
+  Key.cols = cols;
+  Key.rows = rows;
+
+  const str = kleStr.replace(/([a-z]):/g, '"$1":');
+  console.log('str', str);
+  const data = JSON.parse('[' + str + ']');
+  const keys = [];
+  console.log('data', data);
+
+  data.forEach((r, y) => {
+    r.forEach((key, x) => {
+      if (!key || (typeof key !== 'object')) {
+        keys.push(new Key(x, y, key));
+      }
+    });
+  });
+
+  const matrix = [];
+  let matrixStr = '';
+  for (let r = 0; r < rows.length; r++) {
+    for (let c = 0; c < cols.length; c++) {
+      const key = keys.find(k => k.matCol === c && k.matRow === r);
+      if (key) {
+        key.name = `k${r.toString(16)}${c.toString(16)}`;
+        matrixStr += `${key.name}, `;
+      } else {
+        matrixStr += `___, `;
+      }
     }
+    matrix.push(`  { ${matrixStr.slice(0, -2)} }, `);
+    matrixStr = '';
   }
-  matrix.push(`  { ${matrixStr.slice(0, -2)} }, `);
-  matrixStr = '';
-}
 
-const layout = [];
-let layoutStr = '';
-data.forEach((r, y) => {
-  r.forEach((key, x) => {
-    const k = keys.find(k => k.posCol === x && k.posRow === y);
-    if (k) {
-      layoutStr += k.name + ', ';
-    }
+  const layout = [];
+  let layoutStr = '';
+  data.forEach((r, y) => {
+    r.forEach((key, x) => {
+      const k = keys.find(k => k.posCol === x && k.posRow === y);
+      if (k) {
+        layoutStr += k.name + ', ';
+      }
+    });
+    layout.push(`  ${layoutStr}`);
+    layoutStr = '';
   });
-  layout.push(`  ${layoutStr}`);
-  layoutStr = '';
-});
 
-console.log(`#pragma once
+  const res = `#pragma once
 
 #include "quantum.h"
 
-#define _x_ KC_NO
+#define ___ KC_NO
 
 #define LAYOUT( \\
-${layout.join(` \\\n`)}
+  ${layout.join(` \\\n  `)}
 ){ \\
-${matrix.join(` \\\n`)}
+  ${matrix.join(` \\\n  `)}
 }
-`);
+`;
+
+  document.getElementById('result').innerText = res;
+}
+
+(function() {
+  const convert = document.getElementById('convert');
+  const rows = document.getElementById('rows');
+  const cols = document.getElementById('cols');
+  const layout = document.getElementById('layout');
+  console.log('layout', layout);
+  convert.onclick = () => run(rows.value, cols.value, layout.value);
+})();
