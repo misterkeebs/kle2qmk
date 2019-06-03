4bf09d3dbb679033612a90b691db8151f260b874
Felipe Coury <felipe.coury@gmail.com>
2019-06-03 13:00:02 -0300

    refactor: extracted Keyboard class

--- index.js
+++ index.js
@@ -1,48 +1,73 @@
 class Key {
-  constructor(x, y, data) {
-    const [col, row] = data.split('\n');
-
-    this.posCol = x;
-    this.posRow = y;
-    this.matCol = Array.from(Key.cols).indexOf(col);
-    this.matRow = Array.from(Key.rows).indexOf(row);
+  constructor(x, y, col, row) {
+    this.x = x;
+    this.y = y;
+    this.col = col;
+    this.row = row;
+    this.name = `k${row.toString(16)}${col.toString(16)}`;
   }
 }
 
-function kleToJSON(layout) {
+class Keyboard {
+  constructor(kleStr) {
+    this.kleLayout = parseKle(kleStr);
+    this._rows = new Set();
+    this._cols = new Set();
+    this.keys = [];
+
+    this.parse();
+  }
+
+  get rows() {
+    return Array.from(this._rows);
+  }
+
+  get cols() {
+    return Array.from(this._cols);
+  }
+
+  keyAt(x, y) {
+    return this.keys.find(k => k.x === x && k.y === y);
+  }
+
+  parse() {
+    this.layout = [];
+    for (let r = 0; r < this.kleLayout.length; r++) {
+      const rowArr = [];
+      const line = this.kleLayout[r];
+      console.log('line', line);
+      for (let c = 0; c < line.length; c++) {
+        const keyStr = line[c];
+        if (typeof keyStr === 'object') {
+          continue;
+        }
+        const [col, row] = keyStr.split('\n');
+        this._cols.add(col);
+        this._rows.add(row);
+
+        const x = this.cols.indexOf(col);
+        const y = this.rows.indexOf(row);
+        const key = new Key(x, y, col, row);
+        this.keys.push(key);
+        rowArr.push(key);
+      }
+      this.layout.push(rowArr);
+    }
+  }
+}
+
+function parseKle(layout) {
   return JSON.parse('[' + layout.replace(/([a-z]):/g, '"$1":') + ']');
 }
 
-function parseKeys(kleStr) {
-  const layout = kleToJSON(kleStr);
-  const keys = [];
-  layout.forEach((r, y) => {
-    r.forEach((key, x) => {
-      if (!key || (typeof key !== 'object')) {
-        keys.push(new Key(x, y, key));
-      }
-    });
-  });
-  return keys;
-}
-
-function run(_rows, _cols, kleStr) {
-  const cols = _cols.split(',').map(x => x.trim());
-  const rows = _rows.split(',').map(x => x.trim());
-
-  Key.cols = cols;
-  Key.rows = rows;
-
-  const data = kleToJSON(kleStr);
-  const keys = parseKeys(data);
-
+function run(keyboard) {
   const matrix = [];
   let matrixStr = '';
-  for (let r = 0; r < rows.length; r++) {
-    for (let c = 0; c < cols.length; c++) {
-      const key = keys.find(k => k.matCol === c && k.matRow === r);
+  for (let r = 0; r < keyboard.rows.length; r++) {
+    for (let c = 0; c < keyboard.cols.length; c++) {
+      const key = keyboard.keyAt(c, r);
       if (key) {
-        key.name = `k${r.toString(16)}${c.toString(16)}`;
+        key.name = `k${key.row.toString(16)}${key.col.toString(16)}`;
         matrixStr += `${key.name}, `;
       } else {
         matrixStr += `___, `;
@@ -54,16 +79,16 @@ function run(_rows, _cols, kleStr) {
 
   const layout = [];
   let layoutStr = '';
-  data.forEach((r, y) => {
-    r.forEach((key, x) => {
-      const k = keys.find(k => k.posCol === x && k.posRow === y);
-      if (k) {
-        layoutStr += k.name + ', ';
-      }
-    });
-    layout.push(`  ${layoutStr}`);
-    layoutStr = '';
-  });
+  // data.forEach((r, y) => {
+  //   r.forEach((key, x) => {
+  //     const k = keys.find(k => k.posCol === x && k.posRow === y);
+  //     if (k) {
+  //       layoutStr += k.name + ', ';
+  //     }
+  //   });
+  //   layout.push(`  ${layoutStr}`);
+  //   layoutStr = '';
+  // });
 
   const res = `#pragma once
 
@@ -85,12 +110,19 @@ function calcRowsCols(kleStr) {
   const keys = [];
 }
 
-(function() {
-  const convertEl = document.getElementById('convert');
+window.onload = () => {
+  const convert = document.getElementById('convert');
   const rows = document.getElementById('rows');
   const cols = document.getElementById('cols');
   const layout = document.getElementById('layout');
 
-  convertEl.onclick = () => run(rows.value, cols.value, layout.value);
-  layout.onblur = () => calcRowsCols(layout.value);
-})();
+  let keyboard;
+
+  layout.onblur = () => {
+    keyboard = new Keyboard(layout.value)
+    rows.value = Array.from(keyboard.rows).join(', ');
+    cols.value = Array.from(keyboard.cols).join(', ');
+  };
+
+  convert.onclick = () => run(keyboard);
+};
